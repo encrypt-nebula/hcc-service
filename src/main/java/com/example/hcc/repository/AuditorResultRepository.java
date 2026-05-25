3b25357cc99fce8adb37fdf61e6427fc480bed3f
encrypt-nebula <harsh.1014.sharma@gmail.com>
2026-05-25 12:22:11 +0530

    feat: Add findByAuditor_Id to AuditorResultRepository

--- src/main/java/com/example/hcc/repository/AuditorResultRepository.java
+++ src/main/java/com/example/hcc/repository/AuditorResultRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface AuditorResultRepository extends JpaRepository<AuditorResult, Long> {
     Optional<AuditorResult> findByWorkUnitId(Long workUnitId);
     Optional<AuditorResult> findByFileId(Long fileId);
+    List<AuditorResult> findByAuditor_Id(Long auditorId);
 }
